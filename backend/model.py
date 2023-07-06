2963465446d1caae9bc381162c61fb7a286df775
LisaBriazkalo <elizavetabr2018@gmail.com>
2023-07-06 18:33:07 +0300

    fourWordsEntity, deleted CategoryEntity,CategoriesEntity

--- backend/model.py
+++ backend/model.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-
+import random
 
 class Categories(BaseModel): 
     name : str
@@ -7,13 +7,7 @@ class Categories(BaseModel):
 class Words(BaseModel): 
     word : str
     translate : str
-    example : str
-
-def CategoryEntity(category) -> dict:
-    return {"name" : category["name"]}
-
-def CategoriesEntity(entity) -> list:
-    return[CategoryEntity(category) for category in entity]        
+    example : str      
 
 def WordEntity(word) -> dict:
     return{
@@ -25,3 +19,24 @@ def WordEntity(word) -> dict:
 
 def WordsEntity(entity) -> list:
     return[WordEntity(item) for item in entity]
+
+def fourWordsEntity(trueWord, words:list) ->dict:
+    arr = random.sample(range(0, 4), 4)
+    for i in range(4):
+        if(arr[i]==0):
+            arr[i]=trueWord["translate"]
+        elif(arr[i]==1):
+            arr[i]=words[0]["translate"]
+        elif(arr[i]==2):
+            arr[i]=words[1]["translate"]
+        elif(arr[i]==3):
+            arr[i]=words[2]["translate"]
+        i+=1
+    return{
+        "word": trueWord["word"],
+        "trueTranslate": trueWord["translate"],
+        "translate0": arr[0],
+        "translate1": arr[1],
+        "translate2": arr[2],
+        "translate3": arr[3]
+    }
