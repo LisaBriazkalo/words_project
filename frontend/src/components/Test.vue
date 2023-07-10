6dc7e38b506d0ff018f4cb5d694ab01d5f70722b
LisaBriazkalo <elizavetabr2018@gmail.com>
2023-07-10 16:43:42 +0300

    test

--- frontend/src/components/Test.vue
+++ frontend/src/components/Test.vue
@@ -5,14 +5,35 @@
           :to="{name: 'dictionary', params: {name:this.categoryName}}"
               ><button >=</button></router-link>
         <h1>Test task</h1>
-      </div>
-      <div>
+      </div> 
+      <h4>{{ numberOfQuestions }}/10</h4>
+      <div id="question">
         <h2>{{ currantlyWord.word }}</h2>
-        <button class="button1" onclick="isRightAnswer(event)">{{ currantlyWord.translate0 }}</button>
-        <button class="button1">{{ currantlyWord.translate1 }}</button>
-        <button class="button1">{{ currantlyWord.translate2 }}</button>
-        <button class="button1">{{ currantlyWord.translate3 }}</button>
+        <div class="options">
+          <button 
+            :id="'answer-option-' + currantlyWord.translate0"
+            class="default-option"
+            @click="selectOption(currantlyWord.translate0)"
+          >{{ currantlyWord.translate0 }}</button>
+          <button 
+            :id="'answer-option-' + currantlyWord.translate1"
+            @click="selectOption(currantlyWord.translate1)"
+            class="default-option"
+            >{{ currantlyWord.translate1 }}</button>
+          <button 
+            :id="'answer-option-' + currantlyWord.translate2"
+            @click="selectOption(currantlyWord.translate2)"
+            class="default-option"
+            >{{ currantlyWord.translate2 }}</button>
+          <button 
+            :id="'answer-option-' + currantlyWord.translate3"
+            @click="selectOption(currantlyWord.translate3)"
+            class="default-option"
+            >{{ currantlyWord.translate3 }}</button>
+          </div>
       </div>
+      <h2>{{ categoryName }}</h2>
+      <button @click="nextTest()">next</button>
     </div>
   </template>
   
@@ -21,6 +42,9 @@
     data(){
         return {
             currantlyWord:{},
+            score: 0,
+            failing:0,
+            numberOfQuestions: 0
         }
     },
     props: {
@@ -30,8 +54,68 @@
       }
     },
     methods:{
+      defaultOptionStyle(){
+        var element = document.getElementById('answer-option-' + this.currantlyWord.translate0);
+          element.style.backgroundColor = 'azure';
+          element = document.getElementById('answer-option-' + this.currantlyWord.translate1);
+          element.style.backgroundColor = 'azure';
+          element = document.getElementById('answer-option-' + this.currantlyWord.translate2);
+          element.style.backgroundColor = 'azure';
+          element = document.getElementById('answer-option-' + this.currantlyWord.translate3);
+          element.style.backgroundColor = 'azure';
+      },
+      dissabledButtons(value){
+        var element = document.getElementById('answer-option-' + this.currantlyWord.translate0);
+          element.disabled=value;
+          element.classList.add('default-option');
+          element = document.getElementById('answer-option-' + this.currantlyWord.translate1);
+          element.disabled=value;
+          element = document.getElementById('answer-option-' + this.currantlyWord.translate2);
+          element.disabled=value;
+          element = document.getElementById('answer-option-' + this.currantlyWord.translate3);
+          element.disabled=value;
+      },
+      nextTest() {
+        if(this.numberOfQuestions!=3){
+          this.getWordData();
+          this.dissabledButtons(false);
+          this.defaultOptionStyle();
+        }
+        else{
+          this.$router.push({
+              name: 'result',
+              params: { categoryName: this.categoryName }
+          });
+          
+        }
+      },
+      selectOption(selected_translate){
+        fetch(`http://localhost:8000/is_true_translate/${this.categoryName}/${this.currantlyWord.word}/${selected_translate}`,{
+              method:'GET',
+              headers: {"Content-Type":"application/json"}
+          })
+          .then(resp=>resp.json())
+          .then(data=>{
+              if(data==true){
+                var element = document.getElementById('answer-option-' + selected_translate);
+                element.style.backgroundColor = 'lightgreen';
+                this.score+=1;
+                this.numberOfQuestions+=1;
+              }
+              else{
+                var element = document.getElementById('answer-option-' + selected_translate);
+                element.style.backgroundColor = 'lightpink'; 
+                this.failing+=1;
+                this.numberOfQuestions+=1;   
+              }
+              this.dissabledButtons(true);
+          })
+          .catch(error=>{console.log(error)})
+
+          
+      },
         getWordData() {
-          fetch(`http://localhost:8000/fourWords/${this.categoryName}/`,{
+          fetch(`http://localhost:8000/fourWords/${this.categoryName}`,{
               method:'GET',
               headers: {"Content-Type":"application/json"}
           })
@@ -49,5 +133,20 @@
   </script>
   
 <style>
-
+#question{
+  text-align: center;
+}
+.options{
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+.default-option{
+  padding: 15px;
+  font-size: 15px;
+  color: black;
+  border-width: 2px;
+  border-style: solid;
+  border-color: blueviolet;
+  background-color: azure;
+}
 </style>
